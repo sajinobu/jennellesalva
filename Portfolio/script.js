21c765561ca4121a0a43208b52cdf8bcca91ed77
sajinobu <jennellesalva1215@gmail.com>
2025-12-14 05:08:53 +0800

    up

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -33,3 +33,26 @@ function updateIcon(theme) {
         themeIcon.classList.add('bx-moon');
     }
 }
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.pages ul li a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        
+        if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
